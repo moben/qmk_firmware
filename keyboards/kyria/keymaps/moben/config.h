2f6de2a64db775cf10d556009f5b778109ad8b87
Benedikt McMullin <benedikt@mcmullin.de>
2021-06-12 00:30:11 +0200

    kyria: Add no-hold layers to miryoku

--- keyboards/kyria/keymaps/moben/config.h
+++ keyboards/kyria/keymaps/moben/config.h
@@ -16,6 +16,11 @@
 
 #pragma once
 
+#define GML COLEMAKDH_NO_HOLD_L
+#define GMR COLEMAKDH_NO_HOLD_R
+#define QML QWERTY_NO_HOLD_L
+#define QMR QWERTY_NO_HOLD_R
+
 #ifdef OLED_DRIVER_ENABLE
   #define OLED_DISPLAY_128X64
 #endif
@@ -23,6 +28,7 @@
 #ifdef RGBLIGHT_ENABLE
   #define RGBLIGHT_LIMIT_VAL 175
   #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_LAYERS
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
@@ -54,9 +60,9 @@ XXX, K20, K21, K22, K23, K24, XXX, XXX,      XXX, XXX, K25, K26, K27, K28, K29, 
      N30, N31, K32, K33, K34,                          K35, K36, K37, N38, N39 \
 ) \
 LAYOUT( \
-XXX, K00, K01, K02, K03, K04,                          K05, K06, K07, K08, K09, XXX, \
-XXX, K10, K11, K12, K13, K14,                          K15, K16, K17, K18, K19, XXX, \
-XXX, K20, K21, K22, K23, K24, XXX, XXX,      XXX, XXX, K25, K26, K27, K28, K29, XXX, \
-               XXX, K32, K33, K34, XXX,      XXX, K35, K36, K37, XXX \
+XXX, K00, K01, K02, K03, K04,                            K05,     K06, K07, K08, K09, XXX, \
+XXX, K10, K11, K12, K13, K14,                            K15,     K16, K17, K18, K19, XXX, \
+XXX, K20, K21, K22, K23, K24, TG(QML), TG(GML), TG(GMR), TG(QMR), K25, K26, K27, K28, K29, XXX, \
+               XXX, K32, K33, K34,     XXX,     XXX,     K35,     K36, K37, XXX \
 )
 #endif
--- keyboards/kyria/keymaps/moben/keymap.c
+++ keyboards/kyria/keymaps/moben/keymap.c
@@ -2,5 +2,39 @@
 
 #include QMK_KEYBOARD_H
 
+#include "manna-harbour_miryoku.h"
+
+enum rgb_layers {
+    RGB_BASE,
+    RGB_GAME_LEFT,
+    RGB_GAME_RIGHT,
+};
+
+const rgblight_segment_t PROGMEM my_ledlayer_base[]       = RGBLIGHT_LAYER_SEGMENTS({0, 20, HSV_PURPLE});
+const rgblight_segment_t PROGMEM my_ledlayer_game_left[]  = RGBLIGHT_LAYER_SEGMENTS({0, 10, HSV_CHARTREUSE});
+const rgblight_segment_t PROGMEM my_ledlayer_game_right[] = RGBLIGHT_LAYER_SEGMENTS({10, 10, HSV_CHARTREUSE});
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = {
+    [RGB_BASE]       = my_ledlayer_base,
+    [RGB_GAME_LEFT]  = my_ledlayer_game_left,
+    [RGB_GAME_RIGHT] = my_ledlayer_game_right,
+    NULL
+    // comment to force clang-tidy to wrap this
+};
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+    rgblight_set_layer_state(RGB_BASE, true);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(RGB_GAME_LEFT, layer_state_cmp(state, GML));
+    rgblight_set_layer_state(RGB_GAME_RIGHT, layer_state_cmp(state, GMR));
+
+    return state;
+}
+
 void suspend_power_down_user() { rgblight_disable_noeeprom(); }
 void suspend_wakeup_init_user() { rgblight_enable_noeeprom(); }
